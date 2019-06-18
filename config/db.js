7018daaf17a663e50d7117af7a551701316e5929
Alex <alexey.grygorenko@gmail.com>
2019-06-18 13:13:30 +0300

    connected DB

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+// Create async function (An asynchronous function is a function which operates asynchronously via the event loop, using an implicit Promise to return its result. But the syntax and structure of your code using async functions is much more like using standard synchronous functions.)
+const connectDB = async () => {
+  try {
+    // The await operator is used to wait for a Promise. It can only be used inside an async function.
+    await mongoose.connect(db, { useNewUrlParser: true });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.log(err);
+    // Exit process with failure;
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express'); //import express
+const connectDB = require('./config/db'); // import our DB;
 
 const app = express(); // create new express app
+
+//Connect Database;
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running')); //get request with callback; respond with 'API Running' when a GET request is made to the homepage
 
