6084f2a096ade36e83d223b11fd292046790f5b9
Alex <alexey.grygorenko@gmail.com>
2019-06-18 12:58:09 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express'); //import express
+
+const app = express(); // create new express app
+
+app.get('/', (req, res) => res.send('API Running')); //get request with callback; respond with 'API Running' when a GET request is made to the homepage
+
+const PORT = process.env.PORT || 5000; // PORT equal whatever is in the environment variable PORT (can be nessery for Heroku), or 5000 if there's nothing there (for local)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); //binds and listens for connections on the specified host and port.
