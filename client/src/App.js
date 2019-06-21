395590d5512bc7998c2aada776fd9f7b9fd5a2d9
Alex <alexey.grygorenko@gmail.com>
2019-06-21 17:31:06 +0300

    add redux and alert component

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,34 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 //import components
-import Navbar from '../src/components/layout/Navbar';
-import Landing from '../src/components/layout/Landing';
-import Register from '../src/components/auth/Register';
-import Login from '../src/components/auth/Login';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          <Switch>
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/login' component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Alert />
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
